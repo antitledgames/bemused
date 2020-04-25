5a98ea893e823ff1f5f24775ca30297f5b1d8249
cione123 <39230840+cione123@users.noreply.github.com>
2020-04-25 18:34:37 +0300

    Sunet

--- Bemused/Assets/UI/MainMenu.cs
+++ Bemused/Assets/UI/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour {
     public void PlayGame()
     {
         //yield return new WaitForSeconds(5);
-        SceneManager.LoadScene("Level01");
+        SceneManager.LoadScene("pc_main");
 
     }
     public void QuitGame ()
