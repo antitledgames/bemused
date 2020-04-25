3a12238cb34dea039ccb9c4cc5670232ce448b18
Damian Mitrofan <damian.mitrofan@gmail.com>
2020-04-25 20:45:52 +0300

    ui

--- Bemused/Assets/UI/MainMenu.cs
+++ Bemused/Assets/UI/MainMenu.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    public void PlayGame()
+    public void PlayGamePlayer1()
     {
         //yield return new WaitForSeconds(5);
-        SceneManager.LoadScene("mainScene");
+        SceneManager.LoadScene("Level01PL1");
+
+    }
+    public void PlayGamePlayer2()
+    {
+        //yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Level01PL2");
 
     }
     public void QuitGame ()
