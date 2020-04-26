80d86aaeef4d5532c966a9d97bb4dbac99c5be9b
Noob5431 <andreibuscan@yahoo.com>
2020-04-26 19:08:34 +0300

    ceva

---
+++ Bemused/Assets/scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    
+    public void loadScene (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
